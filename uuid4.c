765ea0475f713ceb7e28dcbd7bd4c8b4098b7671
Igor Sementsov <kuzmich@power-mac-g5-igor-sementsov.local>
2025-09-24 07:30:20 +1100

    uuid4 fix

--- uuid4.c
+++ uuid4.c
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2018 rxi
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the MIT license. See LICENSE for details.
+ * File              : uuid4.c
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 22.09.2025
+ * Last Modified Date: 22.09.2025
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
 #include <stdio.h>
-#include <stdint.h>
 
 #if defined(_WIN32)
 #include <windows.h>
@@ -16,13 +16,12 @@
 #include "uuid4.h"
 
 
-static uint64_t seed[2];
+static int seed[4];
 
-
-static uint64_t xorshift128plus(uint64_t *s) {
+static int xorshift128plus(int *s) {
   /* http://xorshift.di.unimi.it/xorshift128plus.c */
-  uint64_t s1 = s[0];
-  const uint64_t s0 = s[1];
+  int s1 = s[0];
+  const int s0 = s[1];
   s[0] = s0;
   s1 ^= s1 << 23;
   s[1] = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
@@ -44,18 +43,22 @@ int uuid4_init(void) {
   }
 
 #elif defined(_WIN32)
-  int res;
-  HCRYPTPROV hCryptProv;
-  res = CryptAcquireContext(
-    &hCryptProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
-  if (!res) {
-    return UUID4_EFAILURE;
-  }
-  res = CryptGenRandom(hCryptProv, (DWORD) sizeof(seed), (PBYTE) seed);
-  CryptReleaseContext(hCryptProv, 0);
-  if (!res) {
-    return UUID4_EFAILURE;
-  }
+  //int res;
+  //HCRYPTPROV hCryptProv;
+  //res = CryptAcquireContext(
+    //&hCryptProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
+  //if (!res) {
+    //return UUID4_EFAILURE;
+  //}
+  //res = CryptGenRandom(hCryptProv, (DWORD) sizeof(seed), (PBYTE) seed);
+  //CryptReleaseContext(hCryptProv, 0);
+  //if (!res) {
+    //return UUID4_EFAILURE;
+  //}
+	seed[0] = rand();
+	seed[1] = rand();
+	seed[2] = rand();
+	seed[3] = rand();
 
 #else
   #error "unsupported platform"
@@ -67,12 +70,14 @@ int uuid4_init(void) {
 void uuid4_generate(char *dst) {
   static const char *template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
   static const char *chars = "0123456789abcdef";
-  union { unsigned char b[16]; uint64_t word[2]; } s;
+  union { unsigned char b[16]; int word[4]; } s;
   const char *p;
   int i, n;
   /* get random */
   s.word[0] = xorshift128plus(seed);
   s.word[1] = xorshift128plus(seed);
+  s.word[2] = xorshift128plus(seed);
+  s.word[3] = xorshift128plus(seed);
   /* build string */
   p = template;
   i = 0;
