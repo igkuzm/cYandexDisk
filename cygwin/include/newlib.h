06e644fcb66ac5f417b09dc7c323b00ed8e676dc
Igor Sementsov <kuzmich@power-mac-g5-igor-sementsov.local>
2025-10-11 13:56:53 +1100

    cygwin pthread

---
+++ cygwin/include/newlib.h
@@ -0,0 +1,16 @@
+/* newlib.h.  Generated automatically by configure.  */
+#ifndef __NEWLIB_H__
+
+#define __NEWLIB_H__ 1
+
+/* EL/IX level */
+/* #undef _ELIX_LEVEL */
+
+/* Newlib version */
+#define _NEWLIB_VERSION "1.11.0"
+
+/* MB_LEN_MAX */
+#define _MB_LEN_MAX 8
+
+#endif /* !__NEWLIB_H__ */
+
